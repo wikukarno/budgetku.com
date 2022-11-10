fc1eac9fa6fbbec3b82345ddf5c30319967c1d7d
wikukarno <prasetyagama2@gmail.com>
2022-11-10 11:22:30 +0700

    update

--- resources/views/admin/portofolio/index.blade.php
+++ resources/views/admin/portofolio/index.blade.php
@@ -17,8 +17,9 @@
                     <div class="col-12 col-lg-4 mb-4">
                         <a href="{{ route('portofolio.edit', $item->id) }}">
                             <div class="card">
-                                <img src="{{ Storage::url($item->thumbnail) }}" class="card-img-top img-fluid"
-                                    alt="foto {{ $item->title }}" style="background-size: cover; height: 15rem">
+                                <img src="{{ Storage::url($item->thumbnail) }}"
+                                    class="card-img-top img-fluid w-100 h-100" alt="foto {{ $item->title }}"
+                                    style="background-size: cover; height: 15rem">
                                 <div class="card-body">
                                     <h5 class="card-title">{{ $item->title }}</h5>
                                     <p class="card-text">{{ $item->kategori }} - {{ $item->created_at->isoFormat('D MMMM
