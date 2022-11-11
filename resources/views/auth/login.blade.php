3bcdace3719a11046ecc746d91701c2120ff91cf
wikukarno <prasetyagama2@gmail.com>
2022-11-11 11:57:09 +0700

    tambah tombol back to dashboard

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,6 +5,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
+                @auth
+                <a href="{{ route('dashboard') }}" class="btn btn-primary btn-block mt-3">Kembali ke Dashboard</a>
+                @endauth
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
