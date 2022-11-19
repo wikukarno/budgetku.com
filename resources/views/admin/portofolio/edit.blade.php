0dd4db14051f1a281710d86a2ef82e2073e5a941
wikukarno <prasetyagama2@gmail.com>
2022-11-19 11:10:59 +0700

    perbarui date

--- resources/views/admin/portofolio/edit.blade.php
+++ resources/views/admin/portofolio/edit.blade.php
@@ -35,7 +35,7 @@
                         <div class="col-4">
                             <div class="form-group">
                                 <label for="published" class="form-label">Tanggal Dibuat</label>
-                                <input type="text" class="form-control" id="published" name="published"
+                                <input type="date" class="form-control" name="published"
                                     value="{{ $portofolio->published }}">
                             </div>
                         </div>
