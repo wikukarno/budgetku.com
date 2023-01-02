f241f0cb769784670e60e246bab5500f6419a4b6
wikukarno <prasetyagama2@gmail.com>
2023-01-02 14:44:09 +0700

    Perbarui Jarak dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -32,7 +32,7 @@
             </div>
         </div>
     </div>
-    <div class="col-xl-3 col-sm-6">
+    <div class="col-xl-3 col-sm-6 mb-4">
         <div class="card">
             <div class="card-body p-3">
                 <div class="row">
