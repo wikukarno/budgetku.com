030fbb1f9799ac1f044bc488992db45cc1345a78
wikukarno <prasetyagama2@gmail.com>
2023-01-02 09:49:58 +0700

    Tambah Statistik Dashboard

--- app/Http/Controllers/Admin/DashboardAdminController.php
+++ app/Http/Controllers/Admin/DashboardAdminController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\CategoryFinance;
+use App\Models\Finance;
+use App\Models\Portofolio;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardAdminController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $portofolios = Portofolio::count();
+        $finances = Finance::where('users_id', Auth::user()->id)->get();
+        $totalPerbulan = $finances->reduce(function ($carry, $item) {
+            return $carry + $item->price;
+        }, 0);
+        $categoryFinances = CategoryFinance::count();
+        return view('admin.dashboard', compact('portofolios', 'finances', 'totalPerbulan', 'categoryFinances'));
     }
 }
