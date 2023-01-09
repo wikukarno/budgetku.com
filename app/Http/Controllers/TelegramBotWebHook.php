b16d97dfef80c0a31b1906af48a0611f6a17467f
wikukarno <prasetyagama2@gmail.com>
2023-01-09 22:35:25 +0700

    Perbarui Webhook Telegram

--- app/Http/Controllers/TelegramBotWebHook.php
+++ app/Http/Controllers/TelegramBotWebHook.php
@@ -23,7 +23,7 @@ class TelegramBotWebHook extends Controller
         $fromFirstName = User::where('telegram_id', $chatId)->first()->name;
         $sekarang = date("Y-m-d H:i:s");
         if (strpos($message, "/start") === 0 || strpos($message, "/mulai") === 0) {
-            $text = "Halo $fromFirstName, Selamat datang di Bot Telegram Saya. \n\n Sekarang jam $sekarang";
+            $text = "Halo $fromFirstName, Selamat datang di Bot Telegram WIKUARNO.ID. \n\n Sekarang jam $sekarang";
             sendText($chatId, $text);
         }
         // daftarkan telegram id
@@ -34,13 +34,17 @@ class TelegramBotWebHook extends Controller
                 $user->telegram_id = $chatId;
                 $user->telegram_username = $fromFirstName;
                 $user->save();
-                $text = "Terima kasih $fromFirstName, akun kamu sudah terdaftar di Bot Telegram Saya.";
+                $text = "Terima kasih $fromFirstName, akun anda berhasil didaftarkan pada Bot Telegram WIKUARNO.ID.";
+                sendText($chatId, $text);
+            } elseif ($user->telegram_id != null) {
+                $text = "Maaf $fromFirstName, akun kamu sudah terdaftar di Bot Telegram WIKUARNO.ID.";
                 sendText($chatId, $text);
             } else {
-                $text = "Maaf $fromFirstName, akun kamu belum terdaftar di Bot Telegram Saya.";
+                $text = "Maaf $fromFirstName, akun kamu belum terdaftar di Bot Telegram WIKUARNO.ID.";
                 sendText($chatId, $text);
             }
         }
+        // cek id
         if (strpos($message, "/id") === 0) {
             $text = "ID kamu adalah $chatId";
             sendText($chatId, $text);
@@ -54,7 +58,7 @@ class TelegramBotWebHook extends Controller
                 $text = "Hallo $fromFirstName, akun anda berhasil kami amankan, \nkamu bisa mengaktifkan kembali akun kamu dengan cara /aktifkan";
                 sendText($chatId, $text);
             } else {
-                $text = "Maaf $fromFirstName, akun kamu belum terdaftar di Bot Telegram Saya.";
+                $text = "Maaf $fromFirstName, akun kamu belum terdaftar di Bot Telegram WIKUARNO.ID.";
                 sendText($chatId, $text);
             }
         }
@@ -65,7 +69,7 @@ class TelegramBotWebHook extends Controller
                 $text = "Hallo $fromFirstName, akun anda berhasil kami aktifkan kembali, \nTerimakasih.";
                 sendText($chatId, $text);
             } else {
-                $text = "Maaf $fromFirstName, akun kamu belum terdaftar di Bot Telegram Saya.";
+                $text = "Maaf $fromFirstName, akun kamu belum terdaftar di Bot Telegram WIKUARNO.ID.";
                 sendText($chatId, $text);
             }
         }
