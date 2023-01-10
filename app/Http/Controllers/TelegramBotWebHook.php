a53011918651a34548ddef99e3d6ab4b72f2734c
wikukarno <prasetyagama2@gmail.com>
2023-01-10 23:25:22 +0700

    Perbarui Query

--- app/Http/Controllers/TelegramBotWebHook.php
+++ app/Http/Controllers/TelegramBotWebHook.php
@@ -60,17 +60,17 @@ class TelegramBotWebHook extends Controller
             }
         }
         // aktifkan akun
-        // if (strpos($message, "/aktifkan") === 0) {
-        //     $user = User::where('telegram_id', $chatId)->onlyTrashed()->first();
-        //     $user->restore();
+        if (strpos($message, "/aktifkan") === 0) {
+            $user = User::where('telegram_id', $chatId)->onlyTrashed()->first();
+            $user->restore();
 
-        //     if ($user) {
-        //         $text = "Hallo $fromFirstName, akun anda berhasil kami aktifkan kembali, \nkamu bisa mengamankan akun kamu dengan cara /amankan";
-        //         sendText($chatId, $text);
-        //     } else {
-        //         $text = "Maaf $fromFirstName, akun kamu belum terdaftar di Bot Telegram WIKUARNO.ID.";
-        //         sendText($chatId, $text);
-        //     }
-        // }
+            if ($user) {
+                $text = "Hallo $fromFirstName, akun anda berhasil kami aktifkan kembali, \nkamu bisa mengamankan akun kamu dengan cara /amankan";
+                sendText($chatId, $text);
+            } else {
+                $text = "Maaf $fromFirstName, akun kamu belum terdaftar di Bot Telegram WIKUARNO.ID.";
+                sendText($chatId, $text);
+            }
+        }
     }
 }
