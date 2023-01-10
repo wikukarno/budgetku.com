0ddf9833405d749a507591779205370f2233be09
wikukarno <prasetyagama2@gmail.com>
2023-01-10 22:40:32 +0700

    Perbarui Query

--- app/Http/Controllers/TelegramBotWebHook.php
+++ app/Http/Controllers/TelegramBotWebHook.php
@@ -61,7 +61,8 @@ class TelegramBotWebHook extends Controller
         }
         // aktifkan akun
         if (strpos($message, "/aktifkan") === 0) {
-            $user = User::withTrashed()->where('telegram_id', $chatId)->restore();
+            $user = User::withTrashed()->where('telegram_id', $chatId);
+            $user->restore();
             if ($user) {
                 $text = "Hallo $fromFirstName, akun anda berhasil kami aktifkan kembali, \nTerimakasih.";
                 sendText($chatId, $text);
