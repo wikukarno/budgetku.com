87afed1fcd8a433a929e7e0a4518f0f7d03d8fa3
wikukarno <prasetyagama2@gmail.com>
2024-01-04 16:58:57 +0700

    update jquery

--- resources/views/includes/admin/scripts.blade.php
+++ resources/views/includes/admin/scripts.blade.php
@@ -1,5 +1,5 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"
-  integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA=="
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+  integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
   crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
@@ -247,4 +247,3 @@
         format:'d.m.Y'
     });
 </script>
-
