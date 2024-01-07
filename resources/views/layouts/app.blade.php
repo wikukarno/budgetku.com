bab17c0e877de0c74551ee8334ae4ca89956feae
wikukarno <prasetyagama2@gmail.com>
2024-01-07 18:11:29 +0700

    Tambah Favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    {{-- pavicon --}}
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" />
     <title>@yield('title')</title>
     @stack('before-styles')
     @include('includes.admin.styles')
