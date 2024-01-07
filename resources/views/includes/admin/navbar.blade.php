7873daea159e7db41b15d638a308d2cffe97ab24
wikukarno <prasetyagama2@gmail.com>
2024-01-07 18:44:19 +0700

    update navbar

--- resources/views/includes/admin/navbar.blade.php
+++ resources/views/includes/admin/navbar.blade.php
@@ -1,19 +1,19 @@
 <!-- partial:partials/_navbar.html -->
 <nav class="navbar p-0 fixed-top d-flex flex-row">
     <div class="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
-        <a class="navbar-brand brand-logo-mini" href="index.html">
-            <img src="assets/images/logo-mini.svg" alt="logo" />
+        <a class="navbar-brand text-white" href="{{ route('keuangan') }}">
+            <b>WIKUKARNO.COM</b>
         </a>
     </div>
-    <div class="navbar-menu-wrapper flex-grow d-flex align-items-stretch">
-        <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+    <div class="navbar-menu-wrapper flex-grow d-flex justify-content-between align-items-stretch">
+        {{-- <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
             <span class="mdi mdi-menu"></span>
-        </button>
+        </button> --}}
         <ul class="navbar-nav navbar-nav-right">
             <li class="nav-item dropdown">
                 <a class="nav-link" href="#">
                     <div class="navbar-profile">
-                        <p class="mb-0 d-none d-sm-block navbar-profile-name">
+                        <p class="mb-0 d-none d-sm-block d-lg-block navbar-profile-name">
                             {{ Auth::user()->name }}
                         </p>
                     </div>
