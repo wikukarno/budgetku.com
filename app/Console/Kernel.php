428afffe0f704657125f5d38a73883f1bfb74014
wikukarno <prasetyagama2@gmail.com>
2024-01-23 15:31:21 +0700

    Update Logic

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,18 +14,28 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule)
     {
+        // $billBulanan = Bill::with('user')
+        //     ->where('siklus_tagihan', 0)
+        //     ->whereDate('jatuh_tempo_tagihan', Carbon::now()->addDays(1)->toDateString())
+        //     ->get();
 
-        $billBulanan = Bill::with('user')
+        // foreach ($billBulanan as $bill) {
+        //     $schedule->call(function () use ($bill) {
+        //         Log::info('Sending email to ' . 'prasetyagama2@gmail.com');
+        //         Mail::to('prasetyagama2@gmail.com')->send(new BillMail($bill));
+        //     })->everyMinute();
+        // }
+
+        $schedule->call(function () {
+            $billsDueTomorrow = Bill::with('user')
             ->where('siklus_tagihan', 0)
-            ->whereDate('jatuh_tempo_tagihan', Carbon::now()->addDays(1)->toDateString())
-            ->get();
+            ->whereDate('jatuh_tempo_tagihan', Carbon::now()->addDay(1)->toDateString())
+                ->get();
 
-        foreach ($billBulanan as $bill) {
-            $schedule->call(function () use ($bill) {
-                Log::info('Sending email to ' . 'prasetyagama2@gmail.com');
+            foreach ($billsDueTomorrow as $bill) {
                 Mail::to('prasetyagama2@gmail.com')->send(new BillMail($bill));
-            })->everyMinute();
-        }
+            }
+        })->everyMinute();
     }
 
     protected function commands()
