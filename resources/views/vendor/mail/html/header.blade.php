9b01a33f4f5e1a4f0cb1f564e2d3648114f7ad6a
Wiku Karno Prasetyagama <59285330+wikukarno@users.noreply.github.com>
2024-02-05 15:31:03 +0700

    Update header.blade.php

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,9 +3,9 @@
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
             @if (trim($slot) === 'WIKUKARNO.COM')
-            <img src="https://api.wikukarno.com/pavicon.ico" class="logo" alt="Logo">
+            <img src="https://api.wikukarno.com/apple-touch-icon.png" class="logo" alt="Logo">
             @else
-            <img src="https://api.wikukarno.com/pavicon.ico" class="logo" alt="logo">
+            <img src="https://api.wikukarno.com/apple-touch-icon.png" class="logo" alt="logo">
             {{-- {{ $slot }} --}}
             @endif
         </a>
