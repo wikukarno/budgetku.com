51199de3f8e9fab5a618f29f94933928dc5ab578
wikukarno <prasetyagama2@gmail.com>
2024-03-17 01:52:49 +0700

    tambah migration sessions

---
+++ database/migrations/2024_03_17_015108_create_sessions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->string('ip_address', 45);
+            $table->text('user_agent');
+            $table->text('payload');
+            $table->integer('last_activity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sessions');
+    }
+};
