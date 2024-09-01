e9f7c3f90886d044efa949cc3401ce9bf6b460e3
wikukarno <perasetyagama2@gmail.com>
2024-09-01 08:02:57 +0700

    Update: add users_id fiield to category_finance table

---
+++ database/migrations/2024_09_01_080122_add_users_id_to_category_finances.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('category_finances', function (Blueprint $table) {
+            $table->unsignedBigInteger('users_id')->after('name_category_finances');
+            $table->foreign('users_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('category_finances', function (Blueprint $table) {
+            //
+        });
+    }
+};
