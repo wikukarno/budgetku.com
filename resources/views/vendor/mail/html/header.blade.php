8c2e7411b78e490271a8201f921f823d96e7c130
wikukarno <prasetyagama2@gmail.com>
2025-01-05 19:42:01 +0700

    fix: header logo email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,12 +2,12 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-{{--            @if (trim($slot) === 'Budgetku')--}}
-{{--            <img src="https://www.budgetku.com/logo.svg" class="logo" alt="Logo">--}}
-{{--            @else--}}
+            @if (trim($slot) === 'BudgetKu')
+            <img src="https://www.budgetku.com/logo.svg" class="logo" alt="Logo">
+            @else
             <img src="https://www.budgetku.com/logo.svg" class="logo" alt="logo">
-            {{-- {{ $slot }} --}}
-{{--            @endif--}}
+             {{ $slot }}
+            @endif
         </a>
     </td>
 </tr>
