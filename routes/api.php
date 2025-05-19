9475259d4bd17440521979b7d31cbc60a943182b
Wiku Karno Prasetyagama <prasetyagama2@gmail.com>
2025-05-19 11:39:26 +0700

    Removes unused API routes and controllers
    Cleans up the API routes by removing unused endpoints and associated
    controller references. Simplifies route definitions and reduces
    potential maintenance overhead.

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,5 @@
 <?php
 
-use App\Http\Controllers\API\AboutsController;
-use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\API\DocumentController;
-use App\Http\Controllers\API\FinancesController;
-use App\Http\Controllers\API\PortofoliosController;
-use App\Http\Controllers\API\ProxyController;
-use App\Http\Controllers\API\SalaryController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,18 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('salary', [SalaryController::class, 'fetch']);
-
-    Route::get('user', [AuthController::class, 'fetch']);
-    Route::post('logout', [AuthController::class, 'logout']);
+    //
 });
-
-// make route with token
-Route::middleware('access-token')
-    ->group(function () {
-        Route::get('email_checkers', [AuthController::class, 'checkEmail']);
-        Route::get('portofolios', [PortofoliosController::class, 'all']);
-        Route::get('abouts', [AboutsController::class, 'all']);
-        Route::get('documents', [DocumentController::class, 'all']);
-        Route::post('login', [AuthController::class, 'login'])->middleware('throttle:3,1');
-    });
