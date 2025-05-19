305fed782a922e41a795b01d2c8fae9e5d2eb514
Wiku Karno Prasetyagama <prasetyagama2@gmail.com>
2025-05-19 11:32:59 +0700

    remove page admin & customer old

--- resources/views/admin/kategori-income/components/style.blade.php
+++
@@ -1,6 +0,0 @@
-<style>
-    table.dataTable tbody tr td {
-        word-wrap: break-word;
-        word-break: break-all;
-    }
-</style>
--- resources/views/includes/admin/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="footer">
-    <div class="d-sm-flex justify-content-center justify-content-sm-between">
-        <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright ©
-            wikukarno.com {{ \Carbon\Carbon::now()->year }} All rights reserved.
-        
-        </span>
-    </div>
-</footer>
